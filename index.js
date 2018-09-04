6d3c3da02a48518d8bf0153dd2d8d9754bfd0747
Drew Showalter <maelstroswim@gmail.com>
2018-09-04 23:43:17 +1000

    :nail_care: style using js-in-css.

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as styled from 'styled-components';
 
 console.log("Look ma'! I'm doin' javascript!");
+
+styled.injectGlobal`
+html, body {
+  margin: 0;
+  padding: 0;
+  width: auto;
+  height: 100vh;
+  color: #C0FFEE;
+  background-color: #000000;
+}
+
+* {
+  box-shadow: 1px 1px 3px palegoldenrod;
+}
+
+h1 {
+  font-family: 'Baloo Tammudu', cursive;
+  text-transform: uppercase;
+  color: #F17;
+}
+`;
 
 ReactDOM.render(
   <h1>
