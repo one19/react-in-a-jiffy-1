c0d26d527cb2e677d92a4743dab372dd96c07d0b
Drew Showalter <maelstroswim@gmail.com>
2018-09-04 22:29:12 +1000

    :sparkles: added react and react-dom

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 console.log("Look ma'! I'm doin' javascript!");
+
+ReactDOM.render(
+  <h1>
+    This is the <del>old</del> <strong>shiny</strong> way
+  </h1>,
+  document.getElementById('root')
+);
